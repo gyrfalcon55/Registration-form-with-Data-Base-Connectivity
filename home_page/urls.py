ff29ea1f1f6f5591e9193a391d593cbf90c15e24
gyrfalcon55 <sjunaid2034@gmail.com>
2024-12-29 21:36:34 +0530

    update and delete feature added

--- home_page/urls.py
+++ home_page/urls.py
@@ -3,5 +3,8 @@ from home_page import views
 
 urlpatterns = [
     path('',views.signup,name="signup"),
-    path('home',views.home,name="home")
+    path('home',views.home,name="home"),
+    path('update/<id>',views.update,name="update"),
+    path('delete/<id>',views.delete,name="deletedata")
+
 ]
--- home_page/views.py
+++ home_page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from home_page.models import Register
 
 def home(request):
@@ -22,6 +22,46 @@ def signup(request):
         print("data saved successfully")
         
     return render(request,"signup.html")
+
+
+
+def update(request,id):
+    if request.method == "POST":
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email= request.POST.get('email')
+        pwd=request.POST.get('password')
+        edit = Register.objects.get(id=id)
+        edit.firstname = firstname
+        edit.lastname = lastname
+        edit.email = email
+        edit.pwd = pwd
+        edit.save()
+        return redirect("/home")
+    data = Register.objects.get(id=id)
+    context = {"data":data}
+    return render(request,"update.html",context)
+
+
+
+def delete(request,id):
+    data = Register.objects.get(id=id)
+    data.delete()
+    return redirect("/home")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Create your views here.
